4785444c85f0518ab4290764b31176264c983091
mutahir <mutahir1993@gmail.com>
2023-02-25 09:39:11 +0530

    adding logging property

--- src/main/java/CustomerMS/Controller/CustomerController.java
+++ src/main/java/CustomerMS/Controller/CustomerController.java
@@ -1,9 +1,12 @@
 package CustomerMS.Controller;
 
 import CustomerMS.Entity.Customer;
+import CustomerMS.MicroserviceApplication;
 import CustomerMS.Repository.CustomerRepository;
 import CustomerMS.Service.CustomerService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +16,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping("/")
 public class CustomerController {
+
+    static Logger logger= LogManager.getLogger(MicroserviceApplication.class);
 
     @Autowired
     CustomerService customerService;
@@ -26,6 +33,7 @@ public class CustomerController {
     @PostMapping("/addCustomer")
     public Customer addCustomer(@RequestBody Customer customerMS)
     {
+        logger.info("Adding customer to list");
         return customerService.save(customerMS);
     }
 
@@ -33,20 +41,24 @@ public class CustomerController {
     @GetMapping("/getCustomerDetails")
     public List<Customer> getCustomerList()
     {
+        logger.info("Displaying all the customers");
         return customerService.fetchCustomersList();
     }
 
+    //searching customer by id
     @GetMapping("/customer/{id}")
     private Customer getCustomers(@PathVariable("id") int id)
     {
+        logger.info("Displaying a specific customers");
         return customerService.getCustomerById(id);
     }
 
-    // Update Operation
+    // Updating the customer details
     @PutMapping("/updateCustomer/{id}")
     public Customer updateCustomer(@RequestBody Customer customer,
                                     @PathVariable("id") int id)
     {
+        logger.info("Updating the customer details");
         return customerService.updateCustomer(
                 customer, id);
     }
@@ -56,6 +68,7 @@ public class CustomerController {
     public String deleteCustomer(@PathVariable("id")
                                  int id)
     {
+        logger.info("Deleting the customer details");
         customerService.deleteCustomer(id);
         return "Deleted Successfully";
     }
@@ -63,6 +76,7 @@ public class CustomerController {
     // searching customer by name
     @GetMapping("/Customer/{name}")
     public ResponseEntity<List<Customer>> getCustomerByName(@PathVariable String name) {
+        logger.info("Searching customer by name");
         return new ResponseEntity<List<Customer>>(customerRepository.findByName(name), HttpStatus.OK);
     }
 
--- src/main/java/CustomerMS/MicroserviceApplication.java
+++ src/main/java/CustomerMS/MicroserviceApplication.java
@@ -1,29 +1,30 @@
 package CustomerMS;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import org.apache.log4j.LogManager;
+
 @SpringBootApplication
 public class MicroserviceApplication {
+
+	static Logger logger= LogManager.getLogger(MicroserviceApplication.class);
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(MicroserviceApplication.class, args);
+
 	}
 
 	@Bean
-	public RestTemplate restTemplate(){
+	public RestTemplate restTemplate() {
 		{
 			return new RestTemplate();
 		}
-	}
-
-
-
-
-
 
 
 	}
+}
